205cb379cbe15eecd2107fc96447627256ce6859
GMGonzaloMata <gonza.mata2003@gmail.com>
2026-06-14 04:53:53 -0300

    actualizaciones movimientos rapiro

--- src/robot/subscriber.py
+++ src/robot/subscriber.py
@@ -77,9 +77,13 @@ def _procesar_mensaje(mensaje: pubsub_v1.subscriber.message.Message, robot: RAPI
 
         elif evento == "rostro_detectado" and identidad_normalizada in IDENTIDADES_CONOCIDAS:
             logger.info("Detectado: %s (%.0f%%)", identidad, confianza)
-            robot.accion_m9()
-            time.sleep(1.0)
             robot.luz_verde()
+            time.sleep(0.5)
+            robot.accion_m9()
+            time.sleep(SEGUNDOS_POSE)
+            robot.movimiento_predefinido(0)
+            time.sleep(0.5)
+            robot.posicion_neutra()
 
         mensaje.ack()
 
